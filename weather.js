270e52fc3709cc0b9c0e0092e8f95bc8117ba6fa
Jsoriano17 <52586265+Jsoriano17@users.noreply.github.com>
2020-01-08 09:23:00 -0700

    fixed the click for temperature

--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ window.addEventListener('load', ()=> {
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
     let temperatureSection = document.querySelector(".temperature");
-    const temperatureSpan  = document.querySelector(".temperatrue span");
+    const temperatureSpan  = document.querySelector(".temperature span");
 
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
